87a47bcacf73c579c7acd2bd3458baea8fb3327b
mrjannajafi <njfymrjan@gmail.com>
2025-07-18 08:08:17 -0700

    added Js code

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-const buttonContainer = document.querySelector('#container')
-const 
+const buttonContainer = document.querySelector("#container");
+const buttons = document.querySelectorAll(".button");
+const text = document.querySelectorAll(".text");
+
+buttonContainer.addEventListener("click", function (e) {
+  // add closest method because if click child ,
+  // target is button and if click ,
+  // parent element log null
+
+  const clicked = e.target.closest(".button");
+  //   this is modern way that name is Gaurd clause
+  // when we want not excute code in some way
+  if (!clicked) return;
+
+  buttons.forEach((t) => t.classList.remove("button--active"));
+  text.forEach((c) => c.classList.remove("text--active"));
+
+  clicked.classList.add("button--active");
+  document
+    .querySelector(`#tab${clicked.dataset.tab}`)
+    .classList.add("text--active");
+
+  console.log(clicked);
+});
